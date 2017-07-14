f66843a257e233b9c0b64057c172b846698b3d48
kadoya.ryo <kadoya.ryo@jp.fujitsu.com>
2017-07-14 15:14:43 +0900

    門谷資産です。

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { WearViewComponent } from './wear-view/wear-view.component';
+import { WearView2Component } from './wear-view2/wear-view2.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  //homeのルーティング
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  //WearViewのルーティング
+  { path: 'wear-view', component: WearViewComponent},
+  //WearView2のルーティング
+  { path: 'wear-view2', component: WearView2Component},
+  //PageNotFoundのルーティング
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WearViewComponent } from './wear-view/wear-view.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WearView2Component } from './wear-view2/wear-view2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WearViewComponent,
+    PageNotFoundComponent,
+    WearView2Component
   ],
   imports: [
     BrowserModule,
